9d3d34cbab5172976d2f3ea7a0e297b7ae6adad5
Niklas Hofmann <niklas.hofmann@everbase.net>
2014-08-13 17:37:27 +0200

    warning added to code comment

--- src/meshlink.h
+++ src/meshlink.h
@@ -640,28 +640,31 @@ extern void meshlink_hint_address(meshlink_handle_t *mesh, meshlink_node_t *node
  *
  *  @param mesh         A handle which represents an instance of MeshLink.
  *  @param edges        A pointer to a previously allocated array of pointers to
- *  			meshlink_edge_t, or NULL in which case MeshLink will
- *  			allocate a new array. The application CANNOT supply an
- *  			array it allocated itself with malloc, but CAN use
- *  			the return value from the previous call to this function
- *  			(which is the preferred way).
+ *                      meshlink_edge_t, or NULL in which case MeshLink will
+ *                      allocate a new array. The application CANNOT supply an
+ *                      array it allocated itself with malloc, but CAN use
+ *                      the return value from the previous call to this function
+ *                      (which is the preferred way).
  *                      The pointers in the array are valid until meshlink_close() is called.
+ *                      ATTENTION: The pointers and values should never be modified
+ *                      by the application!!!
  *  @param nmemb        A pointer to a variable holding the number of nodes that
- *  			are stored in the array. In case the @a nodes @a
- *  			argument is not NULL, MeshLink might call realloc()
- *  			on the array to change its size.
+ *                      are stored in the array. In case the @a nodes @a
+ *                      argument is not NULL, MeshLink might call realloc()
+ *                      on the array to change its size.
  *                      The contents of this variable will be changed to reflect
  *                      the new size of the array.
- *  
  *  @return             A pointer to an array containing pointers to all known 
- *  			edges, or NULL in case of an error.
- *  			If the @a edges @a argument was not NULL, then the
- *  			retun value can be either the same value or a different
- *  			value. If the new values is NULL, then the old array
- *  			will have been freed by Meshlink.
- *			The caller must call free() on each element of this
- *  			array (but not the contents of said elements),
- *  			as well as the array itself when it is finished.
+ *                      edges, or NULL in case of an error.
+ *                      If the @a edges @a argument was not NULL, then the
+ *                      retun value can be either the same value or a different
+ *                      value. If the new values is NULL, then the old array
+ *                      will have been freed by Meshlink.
+ *                      The caller must call free() on each element of this
+ *                      array (but not the contents of said elements),
+ *                      as well as the array itself when it is finished.
+ *                      ATTENTION: The pointers and values should never be modified
+ *                      by the application!!!
  */
 extern meshlink_edge_t **meshlink_get_all_edges_state(meshlink_handle_t *mesh, meshlink_edge_t **edges, size_t *nmemb);
 
