b15173bd344d57e19e7c95c03b7d5ef9726f7ad1
Aaron Krebs <aaron.krebs@everbase.net>
2016-08-25 18:25:55 -0600

    Do not abort if errors may be recoverable in calling function.

--- src/sptps.c
+++ src/sptps.c
@@ -465,13 +465,11 @@ static bool sptps_receive_data_datagram(sptps_t *s, const void *vdata, size_t le
 		if(!s->instate)
 			return error(s, EIO, "Application record received before handshake finished");
 		if(!s->receive_record(s->handle, type, buffer + 1, len - 21)) {
-			error(s, EIO, "Error: sptps_receive_data_datagram !s->receive_record()");
-			abort();
+			return error(s, EIO, "Error: sptps_receive_data_datagram !s->receive_record()");
 		}
 	} else if(type == SPTPS_HANDSHAKE) {
 		if(!receive_handshake(s, buffer + 1, len - 21)) {
-			error(s, EIO, "Error: sptps_receive_data_datagram !receive_handshake()");
-			abort();
+			return error(s, EIO, "Error: sptps_receive_data_datagram !receive_handshake()");
 		}
 	} else {
 		return error(s, EIO, "Invalid record type %d", type);
