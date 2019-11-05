meshlink/meshlink

commit fa05f996c5500c056a36c1d43e33a407f876643c

Add support for encrypted storage.
This is a large overhaul of how configuration files are handled. All files
are now in PackMessage format, and are read from disk in to memory in one
go, and also saved to disk from memory in one go, using functions in conf.c.

src/net_setup.c
@@ -27,257 +27,205 @@
 #include "meshlink_internal.h"
 #include "net.h"
 #include "netutl.h"
+#include "packmsg.h"
 #include "protocol.h"
 #include "route.h"
 #include "utils.h"
 #include "xalloc.h"
 #include "submesh.h"
 
-bool node_read_ecdsa_public_key(meshlink_handle_t *mesh, node_t *n) {
-	if(ecdsa_active(n->ecdsa)) {
-		return true;
+/// Helper function to start parsing a host config file
+static bool node_get_config(meshlink_handle_t *mesh, node_t *n, config_t *config, packmsg_input_t *in) {
+	if(!config_read(mesh, n->name, config)) {
+		return false;
 	}
 
-	splay_tree_t *config_tree;
-	char *p;
+	in->ptr = config->buf;
+	in->len = config->len;
 
-	init_configuration(&config_tree);
+	uint32_t version = packmsg_get_uint32(in);
 
-	if(!read_host_config(mesh, config_tree, n->name)) {
-		goto exit;
+	if(version != MESHLINK_CONFIG_VERSION) {
+		config_free(config);
+		return false;
 	}
 
-	/* First, check for simple ECDSAPublicKey statement */
+	const char *name;
+	uint32_t len = packmsg_get_str_raw(in, &name);
 
-	if(get_config_string(lookup_config(config_tree, "ECDSAPublicKey"), &p)) {
-		n->ecdsa = ecdsa_set_base64_public_key(p);
-		free(p);
+	if(len != strlen(n->name) || strncmp(name, n->name, len)) {
+		config_free(config);
+		return false;
 	}
 
-exit:
-	exit_configuration(&config_tree);
-	return n->ecdsa;
+	return true;
 }
 
-bool read_ecdsa_public_key(meshlink_handle_t *mesh, connection_t *c) {
-	if(ecdsa_active(c->ecdsa)) {
-		return true;
+/// Read device class, blacklist status and submesh from a host config file. Used at startup when reading all host config files.
+bool node_read_partial(meshlink_handle_t *mesh, node_t *n) {
+	config_t config;
+	packmsg_input_t in;
+
+	if(!node_get_config(mesh, n, &config, &in)) {
+		return false;
 	}
 
-	char *p;
+	char *submesh_name = packmsg_get_str_dup(&in);
 
-	if(!c->config_tree) {
-		init_configuration(&c->config_tree);
+	if(!strcmp(submesh_name, CORE_MESH)) {
+		free(submesh_name);
+		n->submesh = NULL;
+	} else {
+		n->submesh = lookup_or_create_submesh(mesh, submesh_name);
+		free(submesh_name);
 
-		if(!read_host_config(mesh, c->config_tree, c->name)) {
+		if(!n->submesh) {
+			config_free(&config);
 			return false;
 		}
 	}
 
-	/* First, check for simple ECDSAPublicKey statement */
+	int32_t devclass = packmsg_get_int32(&in);
+	bool blacklisted = packmsg_get_bool(&in);
+	config_free(&config);
 
-	if(get_config_string(lookup_config(c->config_tree, "ECDSAPublicKey"), &p)) {
-		c->ecdsa = ecdsa_set_base64_public_key(p);
-		free(p);
-		return c->ecdsa;
-	}
-
-	return false;
-}
-
-bool read_ecdsa_private_key(meshlink_handle_t *mesh) {
-	FILE *fp;
-	char filename[PATH_MAX];
-
-	snprintf(filename, PATH_MAX, "%s" SLASH "ecdsa_key.priv", mesh->confbase);
-	fp = fopen(filename, "rb");
-
-	if(!fp) {
-		logger(mesh, MESHLINK_ERROR, "Error reading ECDSA private key file: %s", strerror(errno));
+	if(!packmsg_input_ok(&in) || devclass < 0 || devclass > _DEV_CLASS_MAX) {
 		return false;
 	}
 
-	mesh->self->connection->ecdsa = ecdsa_read_pem_private_key(fp);
-	fclose(fp);
-
-	if(!mesh->self->connection->ecdsa) {
-		logger(mesh, MESHLINK_ERROR, "Reading ECDSA private key file failed: %s", strerror(errno));
-	}
-
-	return mesh->self->connection->ecdsa;
+	n->devclass = devclass;
+	n->status.blacklisted = blacklisted;
+	return true;
 }
 
-static bool read_invitation_key(meshlink_handle_t *mesh) {
-	FILE *fp;
-	char filename[PATH_MAX];
-
-	if(mesh->invitation_key) {
-		ecdsa_free(mesh->invitation_key);
-		mesh->invitation_key = NULL;
+/// Read the public key from a host config file. Used whenever we need to start an SPTPS session.
+bool node_read_public_key(meshlink_handle_t *mesh, node_t *n) {
+	if(ecdsa_active(n->ecdsa)) {
+		return true;
 	}
 
-	snprintf(filename, PATH_MAX, "%s" SLASH "invitations" SLASH "ecdsa_key.priv", mesh->confbase);
+	config_t config;
+	packmsg_input_t in;
 
-	fp = fopen(filename, "rb");
-
-	if(fp) {
-		mesh->invitation_key = ecdsa_read_pem_private_key(fp);
-		fclose(fp);
-
-		if(!mesh->invitation_key) {
-			logger(mesh, MESHLINK_ERROR, "Reading ECDSA private key file `%s' failed: %s", filename, strerror(errno));
-		}
+	if(!node_get_config(mesh, n, &config, &in)) {
+		return false;
 	}
 
-	return mesh->invitation_key;
+	packmsg_skip_element(&in); /* submesh */
+	packmsg_get_int32(&in); /* devclass */
+	packmsg_get_bool(&in); /* blacklisted */
+
+	const void *key;
+	uint32_t len = packmsg_get_bin_raw(&in, &key);
+
+	if(len != 32) {
+		config_free(&config);
+		return false;
+	}
+
+	n->ecdsa = ecdsa_set_public_key(key);
+	config_free(&config);
+	return true;
 }
 
-bool node_read_devclass(meshlink_handle_t *mesh, node_t *n) {
-
-	splay_tree_t *config_tree;
-	char *p;
-
-	init_configuration(&config_tree);
-
-	if(!read_host_config(mesh, config_tree, n->name)) {
-		goto exit;
+/// Read the full host config file. Used whenever we need to start an SPTPS session.
+bool node_read_full(meshlink_handle_t *mesh, node_t *n) {
+	if(n->canonical_address) {
+		return true;
 	}
 
-	if(get_config_string(lookup_config(config_tree, "DeviceClass"), &p)) {
-		n->devclass = atoi(p);
-		free(p);
+	config_t config;
+	packmsg_input_t in;
+
+	if(!node_get_config(mesh, n, &config, &in)) {
+		return false;
 	}
 
-	if((int)n->devclass < 0 || n->devclass > _DEV_CLASS_MAX) {
-		n->devclass = _DEV_CLASS_MAX;
-	}
+	char *submesh_name = packmsg_get_str_dup(&in);
 
-exit:
-	exit_configuration(&config_tree);
-	return n->devclass != 0;
-}
-
-bool node_read_submesh(meshlink_handle_t *mesh, node_t *n) {
-
-	splay_tree_t *config_tree;
-	char *p;
-
-	init_configuration(&config_tree);
-
-	if(!read_host_config(mesh, config_tree, n->name)) {
-		goto exit;
-	}
-
-	if(get_config_string(lookup_config(config_tree, "SubMesh"), &p)) {
+	if(!strcmp(submesh_name, CORE_MESH)) {
+		free(submesh_name);
 		n->submesh = NULL;
-
-		for list_each(submesh_t, s, mesh->submeshes) {
-			if(!strcmp(p, s->name)) {
-				n->submesh = s;
-				break;
-			}
-		}
+	} else {
+		n->submesh = lookup_or_create_submesh(mesh, submesh_name);
+		free(submesh_name);
 
 		if(!n->submesh) {
-			n->submesh = (submesh_t *)meshlink_submesh_open(mesh, p);
+			return false;
 		}
-
-		free(p);
 	}
 
-exit:
-	exit_configuration(&config_tree);
-	return n->submesh != NULL;
-}
+	packmsg_get_int32(&in); /* devclass */
+	packmsg_get_bool(&in); /* blacklisted */
+	const void *key;
+	uint32_t len = packmsg_get_bin_raw(&in, &key); /* public key */
 
-bool node_read_blacklist_status(meshlink_handle_t *mesh, node_t *n) {
-
-	splay_tree_t *config_tree;
-	bool blacklist_status;
-
-	init_configuration(&config_tree);
-
-	if(!read_host_config(mesh, config_tree, n->name)) {
-		goto exit;
-	}
-
-	if(get_config_bool(lookup_config(config_tree, "blacklisted"), &blacklist_status)) {
-		n->status.blacklisted = blacklist_status;
-	}
-
-exit:
-	exit_configuration(&config_tree);
-	return n->status.blacklisted;
-}
-
-bool node_write_devclass(meshlink_handle_t *mesh, node_t *n) {
-
-	if((int)n->devclass < 0 || n->devclass > _DEV_CLASS_MAX) {
+	if(len != 32) {
 		return false;
 	}
 
-	bool result = false;
-
-	splay_tree_t *config_tree;
-	init_configuration(&config_tree);
-
-	// ignore read errors; in case the file does not exist we will create it
-	read_host_config(mesh, config_tree, n->name);
-
-	config_t *cnf = lookup_config(config_tree, "DeviceClass");
-
-	if(!cnf) {
-		cnf = new_config();
-		cnf->variable = xstrdup("DeviceClass");
-		config_add(config_tree, cnf);
+	if(!ecdsa_active(n->ecdsa)) {
+		n->ecdsa = ecdsa_set_public_key(key);
 	}
 
-	set_config_int(cnf, n->devclass);
+	n->canonical_address = packmsg_get_str_dup(&in);
 
-	if(!write_host_config(mesh, config_tree, n->name)) {
-		goto fail;
+	uint32_t count = packmsg_get_array(&in);
+
+	if(count > 5) {
+		count = 5;
 	}
 
-	result = true;
+	for(uint32_t i = 0; i < count; i++) {
+		n->recent[i] = packmsg_get_sockaddr(&in);
+	}
 
-fail:
-	exit_configuration(&config_tree);
-	return result;
+	config_free(&config);
+
+	return packmsg_done(&in);
 }
 
-bool node_write_submesh(meshlink_handle_t *mesh, node_t *n) {
+bool node_write_config(meshlink_handle_t *mesh, node_t *n) {
+	uint8_t buf[4096];
+	packmsg_output_t out = {buf, sizeof(buf)};
 
-	if(!n->submesh) {
+	packmsg_add_uint32(&out, MESHLINK_CONFIG_VERSION);
+	packmsg_add_str(&out, n->name);
+	packmsg_add_str(&out, n->submesh ? n->submesh->name : CORE_MESH);
+	packmsg_add_int32(&out, n->devclass);
+	assert(n->devclass != 3);
+	packmsg_add_bool(&out, n->status.blacklisted);
+
+	if(ecdsa_active(n->ecdsa)) {
+		packmsg_add_bin(&out, ecdsa_get_public_key(n->ecdsa), 32);
+	} else {
+		packmsg_add_bin(&out, "", 0);
+	}
+
+	packmsg_add_str(&out, n->canonical_address ? n->canonical_address : "");
+
+	uint32_t count = 0;
+
+	for(uint32_t i = 0; i < 5; i++) {
+		if(n->recent[i].sa.sa_family) {
+			count++;
+		} else {
+			break;
+		}
+	}
+
+	packmsg_add_array(&out, count);
+
+	for(uint32_t i = 0; i < count; i++) {
+		packmsg_add_sockaddr(&out, &n->recent[i]);
+	}
+
+	if(!packmsg_output_ok(&out)) {
 		return false;
 	}
 
-	bool result = false;
-
-	splay_tree_t *config_tree;
-	init_configuration(&config_tree);
-
-	// ignore read errors; in case the file does not exist we will create it
-	read_host_config(mesh, config_tree, n->name);
-
-	config_t *cnf = lookup_config(config_tree, "SubMesh");
-
-	if(!cnf) {
-		cnf = new_config();
-		cnf->variable = xstrdup("SubMesh");
-		config_add(config_tree, cnf);
-	}
-
-	set_config_string(cnf, n->submesh->name);
-
-	if(!write_host_config(mesh, config_tree, n->name)) {
-		goto fail;
-	}
-
-	result = true;
-
-fail:
-	exit_configuration(&config_tree);
-	return result;
+	config_t config = {buf, packmsg_output_size(&out, buf)};
+	return config_write(mesh, n->name, &config);
 }
 
 void load_all_nodes(meshlink_handle_t *mesh) {
@@ -301,52 +249,16 @@ void load_all_nodes(meshlink_handle_t *mesh) {
 		node_t *n = lookup_node(mesh, ent->d_name);
 
 		if(n) {
-			if(n == mesh->self && !n->submesh) {
-				node_read_submesh(mesh, n);
-			}
-
 			continue;
 		}
 
 		n = new_node();
 		n->name = xstrdup(ent->d_name);
-		node_read_devclass(mesh, n);
-		node_read_submesh(mesh, n);
-		node_read_blacklist_status(mesh, n);
+		node_read_partial(mesh, n);
 		node_add(mesh, n);
 	}
 
 	closedir(dir);
-}
-
-
-char *get_name(meshlink_handle_t *mesh) {
-	char *name = NULL;
-
-	get_config_string(lookup_config(mesh->config, "Name"), &name);
-
-	if(!name) {
-		return NULL;
-	}
-
-	if(!check_id(name)) {
-		logger(mesh, MESHLINK_ERROR, "Invalid name for mesh->self!");
-		free(name);
-		return NULL;
-	}
-
-	return name;
-}
-
-bool setup_myself_reloadable(meshlink_handle_t *mesh) {
-	mesh->localdiscovery = true;
-	keylifetime = 3600; // TODO: check if this can be removed as well
-	mesh->maxtimeout = 900;
-	mesh->self->options |= OPTION_PMTU_DISCOVERY;
-
-	read_invitation_key(mesh);
-
-	return true;
 }
 
 /*
@@ -443,67 +355,12 @@ static bool add_listen_address(meshlink_handle_t *mesh, char *address, bool bind
   Configure node_t mesh->self and set up the local sockets (listen only)
 */
 bool setup_myself(meshlink_handle_t *mesh) {
-	char *name;
-	char *address = NULL;
+	/* Set some defaults */
 
-	if(!(name = get_name(mesh))) {
-		logger(mesh, MESHLINK_ERROR, "Name for MeshLink instance required!");
-		return false;
-	}
-
-	mesh->self = new_node();
-	mesh->self->connection = new_connection();
-	mesh->self->name = name;
-	mesh->self->devclass = mesh->devclass;
-	mesh->self->connection->name = xstrdup(name);
-	read_host_config(mesh, mesh->config, name);
-
-	if(!get_config_string(lookup_config(mesh->config, "Port"), &mesh->myport)) {
-		int port = check_port(mesh);
-
-		if(port == 0) {
-			return false;
-		}
-
-		xasprintf(&mesh->myport, "%d", port);
-	}
-
-	mesh->self->connection->options = 0;
-	mesh->self->connection->protocol_major = PROT_MAJOR;
-	mesh->self->connection->protocol_minor = PROT_MINOR;
-
-	mesh->self->options |= PROT_MINOR << 24;
-
-	if(!read_ecdsa_private_key(mesh)) {
-		return false;
-	}
-
-	/* Ensure mesh->myport is numeric */
-
-	if(!atoi(mesh->myport)) {
-		struct addrinfo *ai = str2addrinfo("localhost", mesh->myport, SOCK_DGRAM);
-		sockaddr_t sa;
-
-		if(!ai || !ai->ai_addr) {
-			return false;
-		}
-
-		free(mesh->myport);
-		memcpy(&sa, ai->ai_addr, ai->ai_addrlen);
-		sockaddr2str(&sa, NULL, &mesh->myport);
-	}
-
-	/* Check some options */
-
-	if(!setup_myself_reloadable(mesh)) {
-		return false;
-	}
-
-	/* Compression */
-
-	// TODO: drop compression in the packet layer?
-	mesh->self->incompression = 0;
-	mesh->self->connection->outcompression = 0;
+	mesh->localdiscovery = true;
+	keylifetime = 3600; // TODO: check if this can be removed as well
+	mesh->maxtimeout = 900;
+	mesh->self->options |= OPTION_PMTU_DISCOVERY;
 
 	/* Done */
 
@@ -512,7 +369,6 @@ bool setup_myself(meshlink_handle_t *mesh) {
 	mesh->self->status.reachable = true;
 	mesh->self->last_state_change = mesh->loop.now.tv_sec;
 
-	node_write_devclass(mesh, mesh->self);
 	node_add(mesh, mesh->self);
 
 	graph(mesh);
@@ -523,7 +379,7 @@ bool setup_myself(meshlink_handle_t *mesh) {
 
 	mesh->listen_sockets = 0;
 
-	if(!add_listen_address(mesh, address, NULL)) {
+	if(!add_listen_address(mesh, NULL, NULL)) {
 		if(strcmp(mesh->myport, "0")) {
 			logger(mesh, MESHLINK_INFO, "Could not bind to port %s, asking OS to choose one for us", mesh->myport);
 			free(mesh->myport);
@@ -533,7 +389,7 @@ bool setup_myself(meshlink_handle_t *mesh) {
 				return false;
 			}
 
-			if(!add_listen_address(mesh, address, NULL)) {
+			if(!add_listen_address(mesh, NULL, NULL)) {
 				return false;
 			}
 		} else {
@@ -585,15 +441,6 @@ void close_network_connections(meshlink_handle_t *mesh) {
 			c->outgoing = NULL;
 			terminate_connection(mesh, c, false);
 		}
-	}
-
-	if(mesh->outgoings) {
-		list_delete_list(mesh->outgoings);
-	}
-
-	if(mesh->self && mesh->self->connection) {
-		terminate_connection(mesh, mesh->self->connection, false);
-		free_connection(mesh->self->connection);
 	}
 
 	for(int i = 0; i < mesh->listen_sockets; i++) {

doc/FILES
@@ -1,33 +1,0 @@
-MeshLink is based on tinc, and tinc had the following structure for its
-directory with configuration files:
-
-/tinc.conf       Main configuration file
-/tinc-up         Script to configure the virtual network interface
-/ecdsa_key.priv  This node's private key
-/hosts/node1     Host configuration file, containing the node's public key,
-                 address, and perhaps other information.
-/hosts/node2     Another host's configuration file,
-/hosts/...       et cetera.
-
-For MeshLink, we don't have any scripts, and neither the application nor the
-user is supposed to read or edit the configuration files in any way. Therefore,
-it might be a good idea to simplify them to the point that we are left
-with:
-
-/meshlink.conf    Contains only the local node's Name and Port.
-/ecdsa_key.priv   Contains the private key, in binary.
-/nodes/node1      Contains only the public key and a list of known addresses.
-/nodes/node2      Another node's key and addresses,
-/nodes/...        et cetera.
-
-Example /meshlink.conf:
-
-Name = foo
-Port = 12345
-
-Example /nodes/foo:
-
-PublicKey = 19fj193f12d1m02dj12089cn
-Address = foo.example.com 18529
-Address = 93.184.216.119 18529
-Address = 2606:2800:220:6d:26bf:1447:1097:aa7 18529

src/chacha-poly1305/chacha-poly1305.c
@@ -100,3 +100,58 @@ bool chacha_poly1305_decrypt(chacha_poly1305_ctx_t *ctx, uint64_t seqnr, const v
 
 	return true;
 }
+
+bool chacha_poly1305_encrypt_iv96(chacha_poly1305_ctx_t *ctx, const uint8_t *seqbuf, const void *indata, size_t inlen, void *outdata, size_t *outlen) {
+	const uint8_t one[4] = { 1, 0, 0, 0 };	/* NB little-endian */
+	uint8_t poly_key[POLY1305_KEYLEN];
+
+	/*
+	 * Run ChaCha20 once to generate the Poly1305 key. The IV is the
+	 * packet sequence number.
+	 */
+	memset(poly_key, 0, sizeof(poly_key));
+	chacha_ivsetup_96(&ctx->main_ctx, seqbuf, NULL);
+	chacha_encrypt_bytes(&ctx->main_ctx, poly_key, poly_key, sizeof(poly_key));
+
+	/* Set Chacha's block counter to 1 */
+	chacha_ivsetup_96(&ctx->main_ctx, seqbuf, one);
+
+	chacha_encrypt_bytes(&ctx->main_ctx, indata, outdata, inlen);
+	poly1305_auth((uint8_t *)outdata + inlen, outdata, inlen, poly_key);
+
+	if (outlen)
+		*outlen = inlen + POLY1305_TAGLEN;
+
+	return true;
+}
+
+bool chacha_poly1305_decrypt_iv96(chacha_poly1305_ctx_t *ctx, const uint8_t *seqbuf, const void *indata, size_t inlen, void *outdata, size_t *outlen) {
+	const uint8_t one[4] = { 1, 0, 0, 0 };	/* NB little-endian */
+	uint8_t expected_tag[POLY1305_TAGLEN], poly_key[POLY1305_KEYLEN];
+
+	/*
+	 * Run ChaCha20 once to generate the Poly1305 key. The IV is the
+	 * packet sequence number.
+	 */
+	memset(poly_key, 0, sizeof(poly_key));
+	chacha_ivsetup_96(&ctx->main_ctx, seqbuf, NULL);
+	chacha_encrypt_bytes(&ctx->main_ctx, poly_key, poly_key, sizeof(poly_key));
+
+	/* Set Chacha's block counter to 1 */
+	chacha_ivsetup_96(&ctx->main_ctx, seqbuf, one);
+
+	/* Check tag before anything else */
+	inlen -= POLY1305_TAGLEN;
+	const uint8_t *tag = (const uint8_t *)indata + inlen;
+
+	poly1305_auth(expected_tag, indata, inlen, poly_key);
+	if (memcmp(expected_tag, tag, POLY1305_TAGLEN))
+		return false;
+
+	chacha_encrypt_bytes(&ctx->main_ctx, indata, outdata, inlen);
+
+	if (outlen)
+		*outlen = inlen;
+
+	return true;
+}

src/chacha-poly1305/chacha-poly1305.h
@@ -12,4 +12,7 @@ extern bool chacha_poly1305_set_key(chacha_poly1305_ctx_t *ctx, const void *key)
 extern bool chacha_poly1305_encrypt(chacha_poly1305_ctx_t *ctx, uint64_t seqnr, const void *indata, size_t inlen, void *outdata, size_t *outlen);
 extern bool chacha_poly1305_decrypt(chacha_poly1305_ctx_t *ctx, uint64_t seqnr, const void *indata, size_t inlen, void *outdata, size_t *outlen);
 
+extern bool chacha_poly1305_encrypt_iv96(chacha_poly1305_ctx_t *ctx, const uint8_t *seqbuf, const void *indata, size_t inlen, void *outdata, size_t *outlen);
+extern bool chacha_poly1305_decrypt_iv96(chacha_poly1305_ctx_t *ctx, const uint8_t *seqbuf, const void *indata, size_t inlen, void *outdata, size_t *outlen);
+
 #endif //CHACHA_POLY1305_H

src/chacha-poly1305/chacha.c
@@ -79,6 +79,14 @@ void chacha_ivsetup(chacha_ctx *x, const uint8_t *iv, const uint8_t *counter)
 	x->input[15] = U8TO32_LITTLE(iv + 4);
 }
 
+void chacha_ivsetup_96(chacha_ctx *x, const uint8_t *iv, const uint8_t *counter)
+{
+	x->input[12] = counter == NULL ? 0 : U8TO32_LITTLE(counter + 0);
+	x->input[13] = U8TO32_LITTLE(iv + 0);
+	x->input[14] = U8TO32_LITTLE(iv + 4);
+	x->input[15] = U8TO32_LITTLE(iv + 8);
+}
+
 void
 chacha_encrypt_bytes(chacha_ctx *x, const uint8_t *m, uint8_t *c, uint32_t bytes)
 {

src/conf.h
@@ -2,8 +2,8 @@
 #define MESHLINK_CONF_H
 
 /*
-    conf.h -- header for conf.c
-    Copyright (C) 2014, 2017 Guus Sliepen <guus@meshlink.io>
+    econf.h -- header for econf.c
+    Copyright (C) 2018 Guus Sliepen <guus@meshlink.io>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -20,34 +20,32 @@
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
-#include "list.h"
-#include "meshlink_internal.h"
-#include "splay_tree.h"
+struct meshlink_handle;
 
 typedef struct config_t {
-	char *variable;
-	char *value;
-	int line;
+	const uint8_t *buf;
+	size_t len;
 } config_t;
 
-extern void init_configuration(struct splay_tree_t **);
-extern void exit_configuration(struct splay_tree_t **);
-extern config_t *new_config(void) __attribute__((__malloc__));
-extern void free_config(config_t *);
-extern void config_add(struct splay_tree_t *, config_t *);
-extern config_t *lookup_config(struct splay_tree_t *, char *);
-extern config_t *lookup_config_next(struct splay_tree_t *, const config_t *);
-extern bool get_config_bool(const config_t *, bool *);
-extern bool get_config_int(const config_t *, int *);
-extern bool set_config_int(config_t *, int);
-extern bool get_config_string(const config_t *, char **);
-extern bool set_config_string(config_t *, const char *);
-extern bool get_config_address(const config_t *, struct addrinfo **);
+//extern bool config_read_file(struct meshlink_handle *mesh, FILE *f, struct config_t *);
+//extern bool config_write_file(struct meshlink_handle *mesh, FILE *f, const struct config_t *);
+extern void config_free(struct config_t *config);
 
-extern bool read_server_config(struct meshlink_handle *mesh);
-extern bool read_host_config(struct meshlink_handle *mesh, struct splay_tree_t *, const char *);
-extern bool write_host_config(struct meshlink_handle *mesh, const struct splay_tree_t *, const char *);
-extern bool modify_config_file(struct meshlink_handle *mesh, const char *name, const char *key, const char *value, int trim);
-extern bool append_config_file(struct meshlink_handle *mesh, const char *name, const char *key, const char *value);
+extern bool config_init(struct meshlink_handle *mesh);
+extern bool config_destroy(const char *confbase);
+
+extern bool main_config_exists(struct meshlink_handle *mesh);
+extern bool main_config_lock(struct meshlink_handle *mesh);
+extern void main_config_unlock(struct meshlink_handle *mesh);
+extern bool main_config_read(struct meshlink_handle *mesh, struct config_t *);
+extern bool main_config_write(struct meshlink_handle *mesh, const struct config_t *);
+
+extern bool config_exists(struct meshlink_handle *mesh, const char *name);
+extern bool config_read(struct meshlink_handle *mesh, const char *name, struct config_t *);
+extern bool config_write(struct meshlink_handle *mesh, const char *name, const struct config_t *);
+
+extern bool invitation_read(struct meshlink_handle *mesh, const char *name, struct config_t *);
+extern bool invitation_write(struct meshlink_handle *mesh, const char *name, const struct config_t *);
+extern size_t invitation_purge_old(struct meshlink_handle *mesh, time_t deadline);
 
 #endif

src/connection.c
@@ -70,10 +70,6 @@ void free_connection(connection_t *c) {
 
 	free(c->name);
 
-	if(c->config_tree) {
-		exit_configuration(&c->config_tree);
-	}
-
 	free(c);
 }
 

commit 91501d27480bf37881b6872cf63462c50de197f3

Handle host config files without a public key.
Commit fa05f996c5500c056a36c1d43e33a407f876643c broke reading config
files for hosts for which no public key is known.

src/net_setup.c
@@ -170,12 +170,14 @@ bool node_read_from_config(meshlink_handle_t *mesh, node_t *n, const config_t *c
 	const void *key;
 	uint32_t len = packmsg_get_bin_raw(&in, &key);
 
-	if(len != 32) {
-		return false;
-	}
+	if(len) {
+		if(len != 32) {
+			return false;
+		}
 
-	if(!ecdsa_active(n->ecdsa)) {
-		n->ecdsa = ecdsa_set_public_key(key);
+		if(!ecdsa_active(n->ecdsa)) {
+			n->ecdsa = ecdsa_set_public_key(key);
+		}
 	}
 
 	n->canonical_address = packmsg_get_str_dup(&in);