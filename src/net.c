09a250a2b1d475700e1d8f18d1a835d17d0483b1
Guus Sliepen <guus@meshlink.io>
2020-04-08 00:46:26 +0200

    Fix a debug message being logged incorrectly.

--- src/net.c
+++ src/net.c
@@ -610,7 +610,7 @@ static void periodic_handler(event_loop_t *loop, void *data) {
 
 	for splay_each(node_t, n, mesh->nodes) {
 		if(n->status.dirty) {
-			if(node_write_config(mesh, n)) {
+			if(!node_write_config(mesh, n)) {
 				logger(mesh, MESHLINK_DEBUG, "Could not update %s", n->name);
 			}
 
