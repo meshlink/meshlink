9984c2d2263bf2a88075b7b381cfe5838441d275
Guus Sliepen <guus@meshlink.io>
2020-07-12 17:31:40 +0200

    Send and receive multicast mDNS packets.
    We don't actually support the full mDNS spec, we just send something that
    passes for a valid mDNS packet, and expect other nodes to send packets back
    with exactly the same format. All other mDNS packets will be ignored.

---
+++ src/mdns.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// SPDX-FileCopyrightText: 2020 Guus Sliepen <guus@meshlink.io>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include <stdint.h>
+#include <unistd.h>
+
+size_t prepare_packet(void *buf, size_t size, const char *name, const char *protocol, const char *transport, uint16_t port, int nkeys, const char **keys, const char **values);
+bool parse_packet(const void *buf, size_t size, char **name, const char *protocol, const char *transport, uint16_t *port, int nkeys, const char **keys, char **values);
+
