3d383945903a482acc17ee0254ee03ea479fe52c
Guus Sliepen <guus@meshlink.io>
2020-08-19 00:35:31 +0200

    Improved mDNS compliance.
    Split the mDNS packets into separate request and response packets, and only
    send responses when we receive a valid request.
    
    It's now possible to find local MeshLink nodes using tools like dns-sd and
    avahi-browse, if told to explicitly search for the _appname._tcp service.

--- src/mdns.h
+++ src/mdns.h
@@ -7,5 +7,8 @@
 #include <unistd.h>
 
 size_t prepare_packet(void *buf, size_t size, const char *name, const char *protocol, const char *transport, uint16_t port, int nkeys, const char **keys, const char **values, bool response);
-bool parse_packet(const void *buf, size_t size, char **name, const char *protocol, const char *transport, uint16_t *port, int nkeys, const char **keys, char **values, bool *response);
+size_t prepare_request(void *buf, size_t size, const char *protocol, const char *transport);
+size_t prepare_response(void *buf, size_t size, const char *name, const char *protocol, const char *transport, uint16_t port, int nkeys, const char **keys, const char **values);
+bool parse_response(const void *buf, size_t size, char **name, const char *protocol, const char *transport, uint16_t *port, int nkeys, const char **keys, char **values);
+bool parse_request(const void *buf, size_t size, const char *protocol, const char *transport);
 
