d9164d1d24e4100e14b269f170ac5266463f2028
Guus Sliepen <guus@meshlink.io>
2021-06-10 22:39:40 +0200

    Remove a warning about setting the UDP address of mesh->self.
    This warning is not useful.

--- src/node.c
+++ src/node.c
@@ -112,7 +112,6 @@ node_t *lookup_node_udp(meshlink_handle_t *mesh, const sockaddr_t *sa) {
 
 void update_node_udp(meshlink_handle_t *mesh, node_t *n, const sockaddr_t *sa) {
 	if(n == mesh->self) {
-		logger(mesh, MESHLINK_WARNING, "Trying to update UDP address of mesh->self!");
 		return;
 	}
 
